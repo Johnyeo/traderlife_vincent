2d9769d0d7f909fd404654c4041c175b3b6ee087
zhangyao <helloenola@gmail.com>
2017-06-15 19:17:59 +0800

    1. Feature: add goodname, price, count. 2. File: create developing guideline.

--- sign/static/js/gamepage.js
+++ sign/static/js/gamepage.js
@@ -2,19 +2,21 @@
  * Created by zhangyao on 2017/6/13.
  */
 
+
 $(document).ready(function () {
 
      var add2order = $('.add2order_btn');
 
      add2order.click(function () {
-         var goodname = $('.add2order_btn').parent().find('.goodname');
-         var count = $(".count");
-         var price = $(".price");
+         var goodname = $(this).parent().find('.goodname');
+         var count = $(this).parent().find(".form-control");
+         var price = $(this).parent().find(".price");
+         //创建一个订单的对象。
          var order = {
-             goodname: goodname.value,
-             count: count.value,
-             price: price.value,
+             goodname: goodname.attr('value'),
+             count: count.val(),
+             price: price.attr('value'),
          };
-         console.log(goodname.attr('value'));
+         console.log(order.goodname, order.count, order.price);
      });
  });
