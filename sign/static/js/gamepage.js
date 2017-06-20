f4c99dd4018fece8249ab2b9840f70970945511c
zhangyao <helloenola@gmail.com>
2017-06-20 15:16:28 +0800

    Add function of dropping product from order.Changing index.

--- sign/static/js/gamepage.js
+++ sign/static/js/gamepage.js
@@ -58,6 +58,7 @@ $(document).ready(function () {
     // 创建一个数组。用来盛放加入订单的产品。 order_total用于计算总额。
     var order = [];
     var order_total = 0;
+    var index = 1
     function click_add_to_order() {
         var add2order = $('.add2order_btn');
         add2order.click(function () {
@@ -78,25 +79,44 @@ $(document).ready(function () {
 
 // TODO 进行重复判断。 如果该元素已经存在，则只加数字，不新增元素
         // 在页面上增加已经加入订单的产品
-        var order_goodname = $("<td></td>").text(good.goodname)
-        var order_goodcount = $("<td></td>").text(good.count)
-        var order_item_del = $("<td></td>").append("<button class='btn btn-xs btn-danger remove_btn' style='width: 60px'>移除</button>")
-        var order_tr_1 = $("<tr>").append(order_goodname, order_goodcount, order_item_del)
-        $(".order_list").append(order_tr_1)
+        var order_index = $("<td class='order_index'>").text(index);
+        var order_goodname = $("<td></td>").text(good.goodname);
+        var order_goodcount = $("<td></td>").text(good.count);
+        var order_item_del = $("<td></td>").append("<button class='btn btn-xs btn-danger remove_btn' style='width: 60px'>移除</button>");
+        var order_tr_1 = $("<tr class='order_tr'>").append(order_index, order_goodname, order_goodcount, order_item_del);
+        $(".order_list").append(order_tr_1);
+        index = index + 1;  // 序号
 
         // 计算总额
-
-        order_total = caculate_total(good.price,good.count, order_total)
-        $(".order_total").append("<span></span>").text(order_total)
+        order_total = caculate_total(good.price,good.count, order_total);
+        $(".order_total").append("<span></span>").text(order_total);
      });
     };
-    
-    click_add_to_order()
-    
-    $('.remove_btn').click(function () {
-        $(this).parent().remove()
-        
-    })
+    click_add_to_order();
+
+    // 实现删除刚刚添加的元素。需要用.on的方法。 才能处理动态添加的元素。
+    $(".order_list").on("click",".remove_btn", function () {
+       // 从order中删除  .text方法获取值，.val获取input 的值， attr('value")获得某个属性的值，比如value
+        var i_str = $(this).parent().parent().find(".order_index").text();
+        order.splice(i_str-1, 1);
+
+        // 从UI上删除
+        $(this).parent().parent().remove();
+
+        // 重排列list的序号
+        index = 1; // 这里如果定义成 var index = 1 就会重新计算。 再加产品数字会继续增加，不正确。
+        // $.each(order,function (i, val) { // 起初想要用循环order 数组，来显示。 但是后来发现并不合适。 （忘了为什么。。。。。）
+            $(".order_tr").each(function () {
+                $(this).find(".order_index").text(index); // 最开头这个this，想了 好久才明白过来。如果不加this，改的就是所有的index
+                index++;
+            })
+        // })
+
+
+       //重新计算总额
+        order_total = caculate_total(good.price,good.count, order_total)
+        $(".order_total").append("<span></span>").text(order_total)
+    });
 
     // 点击提交，把对象提交
     $('#submit_order').click(function () {
