59323e41160e833550c8fbce5c1402e27c6035df
zhangyao <helloenola@gmail.com>
2017-06-21 11:32:45 +0800

    set the favicon.

--- traderlife/urls.py
+++ traderlife/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 from sign import views
 
 urlpatterns = [
@@ -25,5 +27,6 @@ urlpatterns = [
     url(r'^event_manage', views.event_manage),
     url(r'^accounts/login/$', views.index),
     url(r'^gamepage', views.gamepage),
-    url(r'^getOrder',views.getOrder)
+    url(r'^getOrder',views.getOrder), # 获取订单的ajax
+    url(r'^favicon.ico$', RedirectView.as_view(url=r'static/favicon.ico')), # 设置favicon
 ]
